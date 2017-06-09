2616adeebbef87b4c467253c431f6c94f61a5599
meetyyj <meetyyj@gmail.com>
2017-06-09 22:53:05 +0800

    add .gitignore for loop

---
+++ primary/forLoop.py
@@ -0,0 +1,14 @@
+names = ['Michael', 'Bob', 'Tracy']
+for name in names:
+    print(name)
+
+sum1 = 0
+for x in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]:
+    sum1 = sum1 + x
+print(sum1)
+
+sum2 = 0
+for x in range(101):
+    sum2 = sum2 + x
+print(sum2)
+
