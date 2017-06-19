aaf8a881032f70e076f0db9df78c856b3a6cf7dd
meetyyj <meetyyj@gmail.com>
2017-06-19 23:49:46 +0800

    learn list

---
+++ primary/learnList.py
@@ -0,0 +1,11 @@
+l = ['Micheal0', 'Sarah1', 'Tracy2', 'Bob3', 'Jack4']
+print(l[0])
+print(l[0:3])
+print(l[:3])
+print(l[1:3])
+print(l[-2:])
+print(l[-2:-1])
+
+
+L = list(range(100))
+print(L[::5])
